9c0d4b060e513788818368503bc9ef4564a25334
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-11-05 19:44:07 -0500

    Dynamic Approach

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "../data";
 import logo from "../logo.svg";
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(true);
+  const [showLinks, setShowLinks] = useState(false);
+  const refListContainer = useRef(null);
+  const refList = useRef(null);
+
+  const toggleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  const linkStyles = {
+    height: showLinks
+      ? refList.current.getBoundingClientRect().height + "px"
+      : "0px",
+  };
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
-          <button
-            onClick={() => setShowLinks(!showLinks)}
-            className="nav-toggle"
-          >
+          <button onClick={toggleShowLinks} className="nav-toggle">
             <FaBars />
           </button>
         </div>
-        
-          <div className={showLinks? "links-container show-container":"links-container"}>
-            <ul className="links">
-              {links.map(({ id, text, url }) => {
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-       
+
+        <div
+          ref={refListContainer}
+          style={linkStyles}
+          className={"links-container"}
+        >
+          <ul className="links" ref={refList}>
+            {links.map(({ id, text, url }) => {
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
         <div className="social-icons"></div>
       </div>
     </nav>
